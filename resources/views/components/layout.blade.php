69b1ab196a04edbe0ab0472aefd388254363cd81
argontis <taufikdeniarto@gmail.com>
2026-08-25 18:39:37 +0700

    Add auto cache-bust script to layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,12 @@
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
+    <script>
+        if(!localStorage.getItem('elc_cache_bust_v2')) {
+            localStorage.setItem('elc_cache_bust_v2', 'true');
+            window.location.reload(true);
+        }
+    </script>
 
     <!-- CSRF Token wajib ada agar script bisa POST ke Laravel -->
     <meta name="csrf-token" content="{{ csrf_token() }}">

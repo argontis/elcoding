16b90ac36cf720909aae2d8c8e467fab23f43086
argontis <argontis@example.com>
2026-09-22 14:26:00 +0700

    fix: use Inertia::location instead of redirect() to properly redirect Inertia login request to Blade dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -56,7 +56,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         if (!$request->user()->isAdminOrMentor()) {
-            return redirect()->route('pkl.dashboard');
+            return Inertia::location(route('pkl.dashboard'));
         }
 
         $intended = $request->session()->pull('url.intended', '/admin');
